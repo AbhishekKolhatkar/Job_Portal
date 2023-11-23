3f19462acd90e01144e8bbc9cb39507744233383
AbhishekKolhatkar <abhishek.kolhatkar2000@gmail.com>
2023-11-23 20:41:37 +0530

    Changed folder structure

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
-const dbConfig = require("./db.js");
+const dbConfig = require("./config/db");
 app.use(express.json());
 
 const jobsRoute = require("./routes/jobsRoute");
